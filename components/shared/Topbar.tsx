de5cbe0bdd7bbf9368ffa7e970e233f0cdf47227
GaryTang <garytang412@gmail.com>
2023-10-24 18:34:50 -0500

    topbar with someof clerk built in components

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs'
 export default function Topbar() {
     return (
         <nav className='topbar'>
@@ -8,6 +9,26 @@ export default function Topbar() {
                 <Image src="/assets/logo.svg" alt='logo' width={28} height={28} />
                 <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
             </Link>
+            <div className='flex items-center gap-1'>
+                <div className='block md:hidden'>
+
+                    <SignedIn>
+                        <SignOutButton>
+                            <div className='flex cursor-pointer'>
+                                <Image src='/assets/logout.svg' alt='logout' width={24} height={24} />
+                            </div>
+                        </SignOutButton>
+                    </SignedIn>
+
+                </div>
+                <OrganizationSwitcher appearance={
+                    {
+                        elements: {
+                            organizationSwitcherTrigger: "py-2 px-4"
+                        }
+                    }
+                } />
+            </div>
         </nav>
     )
 }
