061aeb37c84e8b63a52e9df5b3040d5d3ad9dea8
GaryTang <garytang412@gmail.com>
2023-10-27 15:51:12 -0500

    onboarding AccountProfile page

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,8 +1,23 @@
 "use client"
 
 
-export default function AccountProfile() {
+interface Props {
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio: string;
+        image: string;
+    };
+    btnTitle: String;
+}
+
+
+const AccountProfile = ({ user, btnTitle }: Props) => {
     return (
         <div className="text-light-1">Account Profile</div>
     )
 }
+
+export default AccountProfile
