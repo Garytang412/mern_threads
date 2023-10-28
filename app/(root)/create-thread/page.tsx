83c537ef8af10560deef9347bdd8fffcdeb3efcb
GaryTang <garytang412@gmail.com>
2023-10-28 09:09:34 -0500

    redirect to onboarding page if not finish account setup

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,15 +1,23 @@
 import { currentUser } from "@clerk/nextjs";
-import { Redirect } from "next";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
 
 const Page = async () => {
     const user = await currentUser();
     if (!user) return null;
 
-    // const userInfo = await fetchUser(user.id);
+    const userInfo = await fetchUser(user.id);
+
+    if (!userInfo?.onboarded) redirect('/onboarding'); // redirect to onboarding page if not finish account setup.
+
+
     return (
-        <h1 className="head-text">
-            create thread
-        </h1>
+        <>
+            <h1 className="head-text">
+                create thread
+            </h1>
+
+        </>
     )
 }
 
